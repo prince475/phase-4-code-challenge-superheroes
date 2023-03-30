4c779930482015fe1a2fa1b84b27104bea7b6925
prince475 <danielmwai475@gmail.com>
2023-03-30 13:44:21 +0300

    Adding a validation to hero_power model which validates a heros average strength.

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,4 +1,14 @@
 class HeroPower < ApplicationRecord
   belongs_to :hero
   belongs_to :power
+
+  validates :strength,
+            {
+              inclusion: {
+                in: %w[Strong Weak Average],
+                message:
+                  "The strength must be either 'Strong', 'Weak' or 'Average'",
+              },
+              presence: true,
+            }
 end
