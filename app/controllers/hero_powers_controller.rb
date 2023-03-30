e889664871a71b7e3b73a28a215df0463b8e7e41
prince475 <danielmwai475@gmail.com>
2023-03-30 13:37:12 +0300

    Updating my hero_powers_controller with a POST/create route and its associated private helper methods

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,22 @@
 class HeroPowersController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+  protect_from_forgery with: :null_session
+
+  def create
+    hero_power = HeroPower.create!(hero_power_params)
+    render json: hero_power.hero, status: :created, serializer: HeroPowerSerializer
+  end
+
+  private
+
+  def hero_power_params
+    params.permit(:strength, :power_id, :hero_id)
+  end
+
+  def render_unprocessable_entity(invalid)
+    render json: {
+             errors: invalid.record.errors.full_messages,
+           },
+           status: :unprocessable_entity
+  end
 end
