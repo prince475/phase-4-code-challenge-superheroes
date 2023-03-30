41c552b10fe519fb11d6447f0bd92094a8c804e8
prince475 <danielmwai475@gmail.com>
2023-03-30 13:39:43 +0300

    Updating my heros_controller with get/index and get/show methods and its private helper method which handles not_found response.

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,18 @@
 class HerosController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+  def index
+    heroes = Hero.all
+    render json: heroes, status: :ok
+  end
+
+  def show
+    hero = Hero.find(params[:id])
+    render json: hero, status: :ok, serializer: HeroPowerSerializer
+  end
+
+  private
+
+  def not_found_response
+    render json: { error: 'Hero not found' }, status: :unprocessable_entity
+  end
 end
